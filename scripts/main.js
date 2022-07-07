34fd1550cd5de3bd7c222581ad886937bce5306a
bea_lazo <beatamayo90@gmail.com>
2022-07-07 13:00:41 +0200

    functions added

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 
 //variables
-const words=["casa", "perro", "gato", "elefante"];
+const words=["casa", "perro", "gato", "elefante","hueso","abeja","pulga","rinoceronte","paloma","herramienta","mosquito"];
 let word="";
 let word_underscores="";
 const full_year=new Date().getFullYear();
 let errors_count=1;
+let wrong_letters=[];
 
 //funciones
 const replaceAt=(word_underscores,index,letter)=>{
@@ -28,9 +29,15 @@ const putWord=()=>{
 }
 const finishGame=(loser_msg,winner_msg)=>{
     errors_count=1;
+    wrong_letters=[];
     document.querySelector("#word_underscores").innerHTML="";
     document.querySelector("#loser_msg").style.display=loser_msg;
     document.querySelector("#winner_msg").style.display=winner_msg; 
+    document.querySelector("#evaluate").disabled=true;
+    putWrongLetters();
+}
+const putWrongLetters=()=>{
+    document.querySelector("#wrong_letters").innerHTML=wrong_letters;
 }
 
 //eventos
@@ -41,33 +48,44 @@ window.onload = ()=>{
 
 document.querySelector("#get_word").addEventListener("click",()=>{
     getWord();
+    document.querySelector("#evaluate").disabled=false;
 })
 
 document.querySelector("#evaluate").addEventListener("click",()=>{
-    const letter=document.querySelector("#letter").value;
-    let letter_found=false;
-    for (const i in word) {
-        if(letter===word[i]){
-            console.log("entre al si")
-            letter_found=true;
-            word_underscores=replaceAt(word_underscores,i*2,letter);
-            putWord();
-        }
-    }
-    if(!letter_found){        
-        if(errors_count<=7){
-            document.querySelector("#hang_img").src="./images/"+(errors_count+1)+".png";
-           
-        }  
-        if(errors_count==7){
-           finishGame("block","none");
-        }      
-      
-        errors_count++;
-    }
-    else{
-        if(word_underscores.indexOf("_")<0){           
-            finishGame("none","block");         
-        }
-    }   
+     const letter=document.querySelector("#letter").value;
+     if(letter!==""){
+        document.querySelector("#empty_input").style.display="none";
+            let letter_found=false;
+            for (const i in word) {
+                if(letter===word[i]){
+                    console.log("entre al si")
+                    letter_found=true;
+                    word_underscores=replaceAt(word_underscores,i*2,letter);
+                    putWord();
+                }
+            }
+            if(!letter_found){        
+                if(errors_count<=7){
+                    document.querySelector("#hang_img").src="./images/"+(errors_count+1)+".png";
+                
+                }  
+                if(errors_count==7){
+                finishGame("block","none");
+                }      
+            
+                errors_count++;
+                wrong_letters.push(letter+" ");
+                putWrongLetters();
+            }
+            else{
+                if(word_underscores.indexOf("_")<0){           
+                    finishGame("none","block");         
+                }
+            }
+
+            document.querySelector("#letter").value="";
+     }
+     else {
+        document.querySelector("#empty_input").style.display="block";
+     }
 })
